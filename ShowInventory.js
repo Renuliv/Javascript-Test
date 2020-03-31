49dce13ba95470407159d48c2acc81d3192dd50a
Renuliv <57868824+Renuliv@users.noreply.github.com>
2020-03-30 20:34:25 -0500

    Update ShowInventory.js

--- ShowInventory.js
+++ ShowInventory.js
@@ -1,20 +1,4 @@
 
-function showInventory() {
-  alert("Hi!");
-  var rawFile = new XMLHttpRequest();
-  rawFile.open("GET","Rosethorne.json",false);
-  rawFile.send();
-  rawFile.onreadystatechange = function() {
-      alert("Hi! 1");
-      if (rawFile.readyState === 4) {
-          alert("Hi! 2");
-          if (rawFile.status === 200 || rawFile.status == 0) {
-              alert(rawFile.responseText);
-          }
-      }
-  }
-}
-
 var rawFile = new XMLHttpRequest();
 rawFile.open("GET","Rosethorne.json",false);
 rawFile.send();
