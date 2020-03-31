57af7ca242cf3b0f0195c1866de40a970a6ad8cd
Renuliv <57868824+Renuliv@users.noreply.github.com>
2020-03-30 20:23:22 -0500

    Update ShowInventory.js

--- ShowInventory.js
+++ ShowInventory.js
@@ -4,7 +4,9 @@ function showInventory() {
   var rawFile = new XMLHttpRequest();
   rawFile.open("GET","Rosethorne.json",false);
   rawFile.onreadystatechange = function() {
+      alert("Hi! 1");
       if (rawFile.readyState === 4) {
+          alert("Hi! 2");
           if (rawFile.status === 200 || rawFile.status == 0) {
               alert(rawFile.responseText);
           }
